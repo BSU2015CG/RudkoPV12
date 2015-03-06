6d4853735baa74284e561650abef8b482d2ff627
Pavel Rudko <icpavelrudko@gmail.com>
2015-03-06 02:38:28 +0200

    Colorpicker modal.

--- scripts/colorpicker.js
+++ scripts/colorpicker.js
@@ -1,8 +1,8 @@
 function initColorPicker(){
-	var canvas = document.getElementById('color-picker');
-    var ctx = canvas.getContext('2d');
+	var canvas = document.getElementById('color-picker'),
+        ctx = canvas.getContext('2d'),
+        image = new Image();
 
-	var image = new Image();
 	image.onload = function () {
 	    ctx.drawImage(image, 0, 0, image.width, image.height);
 	}
@@ -20,17 +20,31 @@ function initColorPicker(){
 	    if(distance >= radius){
 	    	return;
 	    }
-
-	    var imageData = ctx.getImageData(x, y, 1, 1);
+	    
+        var imageData = ctx.getImageData(x, y, 1, 1);
         var pixel = imageData.data;
 
         CurrentColor.setColor(pixel[0], pixel[1], pixel[2]);
     }, false);
-}
+};
 
 function getRelativeCoords(event){
     if (event.offsetX !== undefined && event.offsetY !== undefined) {
     	{ return { x: event.offsetX, y: event.offsetY }; }
     }
     return { x: event.layerX, y: event.layerY };
-}
+};
+
+function initColorPickerModal(){
+    function setColorPickerHidden(hidden){
+        document.getElementById("color-picker-wrap").setAttribute('data-hidden', hidden);
+    }
+
+    document.getElementById('color-picker-button').onclick = function(){
+        setColorPickerHidden('false');
+    }
+
+    document.getElementById('color-picker-wrap').addEventListener('click', function(){
+        setColorPickerHidden('true');
+    }, false);
+};
